6e2c9cbdc1e79dbc1d4a5b4116422030460f5b68
jaspreetScripts <119395932+jaspreetScripts@users.noreply.github.com>
2023-06-22 13:44:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+const p= document.createElement("p");
+
+
+function updateTime(){
+	const currentDate= new Date();
+	const month= currentDate.getMonth();
+	const date= currentDate.getDate();
+	const year = currentDate.getYear();
+	const time = currentDate.toLocaleTimeString();
+
+	let datetime= month+"/"+date+"/"+year+"/"+time;
+	p.innerText=datetime;
+	document.body.appendChild(p);
+}
+
+updateTime();
+
+setInterval(updateTime,1000);
